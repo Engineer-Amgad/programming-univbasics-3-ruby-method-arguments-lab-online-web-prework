efee5fe9ec3e46710aa22bdcf709491df35da60c
fearless-jest-9632 <amgadmalik@yahoo.com>
2019-09-02 00:36:27 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,7 +8,12 @@ Def untroduction (name)
     
 end
 
+Def introduction_with_language (name, language)
+    puts "Hi my name is #{name} and I am learning to program in #{language}."
+end
 
+Def introduction_with_language_optional (name, language = "Ruby")
+    
 
 
 
